7711418d17ea28b38b69d0eba990c714d7b47577
FreudCat <43690482+FreudCat@users.noreply.github.com>
2025-11-22 14:52:45 -0800

    Add Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Link } from '@tanstack/react-router'
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const navLinks = [
+    { name: 'Home', path: '/' },
+    { name: 'Contact', path: '/contact' },
+    { name: 'Portfolio', path: '/portfolio' },
+    { name: 'Game', path: '/path' }
+  ]
+
+  return (
+    <header className={`header-wrapper ${isMenuOpen ? 'is-open' : ''}`}>
+      <Link to="/" className='logo'>
+        MyApp
+      </Link>
+      <nav className='nav-wrapper'>
+        {navLinks.map(link => (
+          <Link
+            key={link.name}
+            to={link.path}
+            className={`nav-link ${link.name}`}
+            activeProps={{ className: 'active' }}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  )
+}
+
+export default Header
