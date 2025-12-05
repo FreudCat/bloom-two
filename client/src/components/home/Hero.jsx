f749761b449515a33ab20225527c27e9f8aab3ac
FreudCat <43690482+FreudCat@users.noreply.github.com>
2025-12-05 07:26:19 -0800

    Add curved background color and restructure hero image and text

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -1,24 +1,32 @@
 const Hero = () => {
   return (
     <section className='hero-wrapper'>
-      <div className='hero-image'></div>
-      <div className='hero-content'>
-        <h1>Hello, I'm Angela</h1>
-        <div className='attributes'>
-          <ul>
-            <li>Lifelong learner</li>
-            <li> | </li>
-            <li>Software Engineer</li>
-            <li> | </li>
-            <li>Silks Enthusiast</li>
-          </ul>
+      <div className='curved-background'>
+        <div className='hero-top-content'>
+          <div className='hero-image'></div>
+          <div className='hero-text'>
+            <h1>Hello, I'm Angela</h1>
+            <div className='attributes'>
+              <ul>
+                <li>Lifelong learner</li>
+                <li> | </li>
+                <li>Software Engineer</li>
+                <li> | </li>
+                <li>Silks Enthusiast</li>
+              </ul>
+            </div>
+            <p>Software Engineer with 3+ years building Vue, PHP, and Ruby apps across 100+ sites and a proven track record mentoring developers and delivering business features.
+            </p>
+          </div>
         </div>
-        <div>
-          <p className='about-me'>Software Engineer with 3+ years building Vue, PHP, and Ruby apps across 100+ sites and a proven track record mentoring developers and delivering business features.</p>
-        </div>
-        <div>
-          <button className='learn-more'><a href='#section-two-truths'>Learn more about me below</a></button>
-        </div>
+        <svg className='curve' viewBox="0 0 1440 100" preserveAspectRatio="none">
+          <path d="M0,0 C480,100 960,100 1440,0 L1440,100 L0,100 Z" fill="#FFFFFF" />
+        </svg>
+      </div>
+
+      <div className='hero-content-card'>
+        <p className='about-me'></p>
+        <button className='learn-more'><a href='#section-two-truths'>Learn more about me below</a></button>
       </div>
     </section>
   )
