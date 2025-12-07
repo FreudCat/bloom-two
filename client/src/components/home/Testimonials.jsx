340dd003a926b6cf5345fbf6dc4383a6403e1851
FreudCat <43690482+FreudCat@users.noreply.github.com>
2025-12-07 12:51:09 -0800

    Add testimonials

--- client/src/components/home/Testimonials.jsx
+++ client/src/components/home/Testimonials.jsx
@@ -6,6 +6,7 @@ import 'swiper/css/navigation'
 const Testimonials = () => {
   return (
     <div className='testimonials-wrapper'>
+      <h2>Testimonials from Colleagues</h2>
       <Swiper
         modules={[Navigation]}
         navigation={true}
@@ -27,40 +28,28 @@ const Testimonials = () => {
         <SwiperSlide>
           <div>
             <p>
-              Lorem ipsum Lorem ipsup. Lorem ipsum. Lorem ipsup
-              Lorem ipsupLorem ipsup
-              Lorem ipsupLorem ipsupLorem ipsup
-              Lorem ipsup2
+              "Angela has been organizing efforts to help the whole frontend team grow our knowledge and skills. She's always so thoughtful, supportive, and just a genuinely great person to work with."
             </p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div>
             <p>
-              Lorem ipsum Lorem ipsup. Lorem ipsum. Lorem ipsup
-              Lorem ipsupLorem ipsup
-              Lorem ipsupLorem ipsupLorem ipsup
-              Lorem ipsup2
+              "Angela consistently exceeds expectations on the Frontend team. She provides timely and thorough responses regarding ongoing development. She is kind, funny, and highly adaptable."
             </p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div>
             <p>
-              Lorem ipsum Lorem ipsup. Lorem ipsum. Lorem ipsup
-              Lorem ipsupLorem ipsup
-              Lorem ipsupLorem ipsupLorem ipsup
-              Lorem ipsup2
+              "Angela is a thorough and diligent code reviewer, testing many edge-cases and giving very high-quality feedback. She is also a natural leader who leads by example, willing to take on challenges and set a high standard."
             </p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div>
             <p>
-              Lorem ipsum Lorem ipsup. Lorem ipsum. Lorem ipsup
-              Lorem ipsupLorem ipsup
-              Lorem ipsupLorem ipsupLorem ipsup
-              Lorem ipsup2
+              "Angela has an amazing eye for detail, she's extremely thorough with her work and is a great communicator."
             </p>
           </div>
         </SwiperSlide>
